9a0d719ef5502474fa3a99d7197f471bc5bd2f83
AnkushSharma2698 <ankush1@ualberta.ca>
2021-03-19 16:14:59 -0600

    done a5

--- sockets.py
+++ sockets.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, jsonify
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -30,6 +30,7 @@ class World:
     def __init__(self):
         self.clear()
         # we've got listeners now!
+        # NOT Exactly sure how to use these, are these intended to be used as clients?
         self.listeners = list()
         
     def add_set_listener(self, listener):
@@ -61,27 +62,81 @@ class World:
 
 myWorld = World()        
 
+# List that holds all current clients of the application
+# List that holds all current clients of the application
+# List that holds all current clients of the application
+
+# Holds each use who connects to the websocket
+# CREDIT: https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
+# This link gave the structure required to handle clients in websockets
+clients = list()
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    # Not sure what to do with this function :(
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect('static/index.html') 
 
-def read_ws(ws,client):
+def send_all(msg):
+    # Store the message in the world
+    # This block is to ensure consistency across instances of the same application.
+    for k,v in json.loads(msg).items():
+        myWorld.set(k, v)
+    # Send the same message to all clients so they are consistent
+    for client in clients:
+        client.put(msg)
+
+def send_all_json(obj):
+    send_all(json.dumps(obj))
+
+
+def read_ws(ws, client):
     '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            msg = ws.receive()
+            print("WS RECV {msg}".format(msg=msg))
+            if msg is not None:
+                packet = json.loads(msg)
+                send_all_json(packet)
+            else:
+                break
+    except:
+        """Done"""
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    # Create the client
+    client = Client()
+    clients.append(client) 
+    g = gevent.spawn(read_ws, ws, client)
+    # Create a gevent listener
+    try:
+        while True:
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:
+        print("WS Error {e}".format(e=e))
+    finally:
+        # Remove the listener
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +154,28 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    body = flask_post_json()
+    for k,v in body.items():
+        myWorld.update(entity, k, v)
+    return flask.jsonify(myWorld.get(entity))
+
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return flask.jsonify(myWorld.world()) 
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return flask.jsonify(myWorld.get(entity)) 
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return flask.jsonify(myWorld.world())
 
 
 
@@ -125,4 +185,4 @@ if __name__ == "__main__":
         and run
         gunicorn -k flask_sockets.worker sockets:app
     '''
-    app.run()
+    os.system("bash run.sh")
